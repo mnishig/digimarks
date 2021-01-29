0538eb3e2c7ecda3432b817c6d7324d2609b97e7
Masahide Nishihama <mnishig@gmail.com>
2021-01-29 09:37:09 +0900

    backup dietpi config /home/dietpi/digimarks/p3 and backup service unit

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Virtualenv to use with the wsgi file (optional)
+#VENV = '/srv/marks.example.com/venv/bin/activate_this.py'
+
+PORT = 8086
+
+DEBUG = False
+
+# Password/url key to do admin stuff with, like adding a user
+# NB: change this to something else! For example, in bash:
+# echo -n "yourstring" | sha1sum
+#SYSTEMKEY = 'S3kr1t'
+SYSTEMKEY = 'aaa'
+
+LOG_LOCATION = 'digimarks.log'
+#LOG_LOCATION = '/var/log/digimarks/digimarks.log'
+# How many logs to keep in log rotation:
+LOG_BACKUP_COUNT = 10
--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,8 @@ activate_this = getattr(settings, 'VENV', None)
 #if activate_this:
 #    execfile(activate_this, dict(__file__=activate_this))
 # Python 3
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
+#with open(activate_this) as file_:
+#    exec(file_.read(), dict(__file__=activate_this))
 
 from digimarks import app as application
 
